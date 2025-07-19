94567d5fd3360557df95a6f83f5c6963e935fd17
barsha-27 <barsha27.sumaiya@gmail.com>
2025-07-19 19:41:56 +0600

    design:brands

--- innopacks/panel/resources/views/brands/form.blade.php
+++ innopacks/panel/resources/views/brands/form.blade.php
@@ -5,7 +5,7 @@
 <x-panel::form.right-btns />
 
 @section('content')
-<div class="card h-min-600">
+<div class="card">
   <div class="card-header">
     <h5 class="card-title mb-0">{{ __('panel/menu.brands') }}</h5>
   </div>
@@ -16,12 +16,54 @@
       @csrf
       @method($brand->id ? 'PUT' : 'POST')
 
-      <x-common-form-input title="{{ __('panel/brand.name') }}" name="name" value="{{ old('name', $brand->name) }}" required placeholder="{{ __('panel/brand.name') }}" />
-      <x-common-form-image title="{{ __('panel/brand.logo') }}" name="logo" value="{{ old('logo', $brand->logo) }}" required />
-      <x-common-form-input title="{{ __('panel/brand.first') }}" name="first" value="{{ old('first', $brand->first) }}" required placeholder="{{ __('panel/brand.first') }}" />
-      <x-common-form-input title="{{ __('panel/common.position') }}" name="position" value="{{ old('position', $brand->position) }}" placeholder="{{ __('panel/common.position') }}" />
-      <x-common-form-switch-radio title="{{ __('panel/common.whether_enable') }}" name="active" :value="old('active', $page->active ?? true)" placeholder="{{ __('panel/common.whether_enable') }}"/>
-      <x-common-form-input title="{{ __('panel/common.slug') }}" name="slug" value="{{ old('slug', $brand->slug) }}" placeholder="{{ __('panel/common.slug') }}" />
+      <div class="row g-3">
+        <!-- Column 1: Name and Initial -->
+        <div class="col-md-6">
+          <x-common-form-input 
+            title="{{ __('panel/brand.name') }}" 
+            name="name" 
+            value="{{ old('name', $brand->name) }}" 
+            required 
+            placeholder="{{ __('panel/brand.name') }}" 
+          />
+          <x-common-form-input 
+            title="{{ __('panel/brand.first') }}" 
+            name="first" 
+            value="{{ old('first', $brand->first) }}" 
+            required 
+            placeholder="{{ __('panel/brand.first') }}" 
+          />
+           <x-common-form-image 
+            title="{{ __('panel/brand.logo') }}" 
+            name="logo" 
+            value="{{ old('logo', $brand->logo) }}" 
+            required 
+          />
+        </div>
+
+        <!-- Column 2: Position, Slug (SEO Alias), Enabled -->
+        <div class="col-md-6">
+          <x-common-form-input 
+            title="{{ __('panel/common.position') }}" 
+            name="position" 
+            value="{{ old('position', $brand->position) }}" 
+            placeholder="{{ __('panel/common.position') }}" 
+          />
+          <x-common-form-input 
+            title="{{ __('panel/common.slug') }}" 
+            name="slug" 
+            value="{{ old('slug', $brand->slug) }}" 
+            placeholder="{{ __('panel/common.slug') }}" 
+          />
+          <x-common-form-switch-radio 
+            title="{{ __('panel/common.whether_enable') }}" 
+            name="active" 
+            :value="old('active', $brand->active ?? true)" 
+            placeholder="{{ __('panel/common.whether_enable') }}"
+          />
+        </div>
+
+      </div>
 
       <button type="submit" class="d-none"></button>
     </form>
